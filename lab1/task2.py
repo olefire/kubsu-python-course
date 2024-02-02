f4ea07d4f1c3307db485e2189b86c470103d5beb
olefire <109245281+olefire@users.noreply.github.com>
2024-02-02 12:24:01 +0300

    fix: switch to snake_case

--- lab1/task2.py
+++ lab1/task2.py
@@ -1,13 +1,13 @@
-def maxProductNotDiv5(x: int):
+def max_product_not_div5(x: int):
     product = 1
     while x != 0:
-        curDigit = x % 10
+        cur_digit = x % 10
         x //= 10
-        if curDigit % 5 != 0:
-            product *= curDigit
+        if cur_digit % 5 != 0:
+            product *= cur_digit
     return product
 
 
 x = int(input())
-print(maxProductNotDiv5(x))
-# print(maxProductNotDiv5(12375)) 42
+print("произведение цифр числа, не делящихся на 5", max_product_not_div5(x))
+# print(max_product_not_div5(12375)) 42
