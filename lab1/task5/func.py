11389ea200cbdb7d2fb6c41c5347fc0f45b27c99
olefire <109245281+olefire@users.noreply.github.com>
2024-02-08 20:43:30 +0300

    fix: redo task5 for string

--- lab1/task5/func.py
+++ lab1/task5/func.py
@@ -3,14 +3,26 @@ def find_dates():
               "августа", "сентября", "октября", "ноября", "декабря"}
 
     dates = []
-    words = input("input text here: ").split()
-    for i in range(len(words) - 2):
-        if words[i].isdigit() and words[i + 1] in months and words[i + 2].isdigit():
-            date = ' '.join(words[i:i+3])
-            dates.append(date)
+    string = input("input text here: ")
+    i = 0
+    while i < len(string):
+        wp = string.find(" ", i)
+        last_index_of_year = i
+        if string[i:wp] in months:
+            if i != 0 and wp != len(string) - 1 and string[i - 3:i].strip().isdigit() and string[wp + 1].isdigit():
+                last_index_of_year = wp + 2
+                while last_index_of_year != len(string) and string[last_index_of_year].isdigit():
+                    last_index_of_year += 1
+
+                date = string[i-3:last_index_of_year].strip()
+                dates.append(date)
+
+        i = last_index_of_year + 1
     return dates
 
 
 dates = find_dates()
 for date in dates:
     print(date)
+
+# "Сегодня 31 февраля 2007 года, а вчера было 15 января 2022 года."
